95a8ce05b91776fa5a647d2e187eeb1fc5a3331f
Kan Fushihara <kan.fushihara@gmail.com>
2016-07-30 16:49:50 +0900

    参加者タイプと設問を指定して一覧表示できるようにしてみた

--- tutorial/retrospectives/aggregate.rb
+++ tutorial/retrospectives/aggregate.rb
@@ -1,25 +1,18 @@
 #!/usr/bin/env ruby
 
-if ARGV.size != 1
-  puts("Usage: #{$0} DIRECTORY")
-  puts(" e.g.: #{$0} 2016-06-09")
+if ARGV.size != 3
+  puts("Usage: #{$0} DIRECTORY TYPE QUESTION")
+  puts(" e.g.: #{$0} 2016-06-09 beginner type")
   exit(false)
 end
 
 require "yaml"
 
-directory = ARGV.first
+directory = ARGV.shift
+type = ARGV.shift
+question = ARGV.shift
 
-# TODO...
-
-beginners = {}
-Dir.glob("#{directory}/beginner-*.yaml") do |beginner_yaml|
-  beginner = YAML.load(File.read(beginner_yaml))
-  p beginner
-end
-
-mentors = {}
-Dir.glob("#{directory}/mentor-*.yaml") do |mentor_yaml|
-  mentor = YAML.load(File.read(mentor_yaml))
-  p mentor
+Dir.glob("#{directory}/#{type}-*.yaml") do |yaml|
+  hash = YAML.load(File.read(yaml))
+  puts hash["questions"][question]
 end
