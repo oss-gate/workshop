33e1596be68898aff0800c221729ef6871015772
Kouhei Sutou <kou@clear-code.com>
2016-11-27 22:56:40 +0900

    Support outputting all answers

--- tutorial/retrospectives/aggregate.rb
+++ tutorial/retrospectives/aggregate.rb
@@ -1,8 +1,9 @@
 #!/usr/bin/env ruby
 
-if ARGV.size != 3
-  puts("Usage: #{$0} DIRECTORY TYPE QUESTION")
-  puts(" e.g.: #{$0} 2016-06-09 beginner type")
+if ARGV.size < 2
+  puts("Usage: #{$0} DIRECTORY TYPE [QUESTION]")
+  puts(" e.g.: #{$0} 2016-06-09 beginner")
+  puts(" e.g.: #{$0} 2016-06-09 beginner motivation")
   exit(false)
 end
 
@@ -10,15 +11,33 @@ require "yaml"
 
 directory = ARGV.shift
 type = ARGV.shift
-question = ARGV.shift
+target_question = ARGV.shift
 
+questionnaires = {}
 Dir.glob("#{directory}/#{type}-*.yaml") do |yaml|
   if File.basename(yaml) =~ /#{type}-(.+)\.yaml/
+    account = $1
     begin
-      hash = YAML.load(File.read(yaml))
-      puts($1 + ": " + hash["questions"][question])
-      puts("=" * 40)
-    rescue => e
+      questionnaires[account] = YAML.load(File.read(yaml))
+    rescue Psych::SyntaxError
+      puts("#{account}: syntax error: #{$!}")
     end
   end
+end
+
+_, key_questionnairy = questionnaires.first
+key_questionnairy["questions"].each do |question, _|
+  unless target_question.nil?
+    next unless question == target_question
+  end
+
+  puts("-" * question.size)
+  puts(question)
+  puts("-" * question.size)
+  questionnaires.each do |account, questionnairy|
+    answer = questionnairy["questions"][question]
+    puts(account + ": " + answer)
+    puts("=" * 40)
+  end
+  puts
 end
