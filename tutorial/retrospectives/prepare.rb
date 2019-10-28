81a47fa6f34680ecc33805f52feb3d296d4c56e3
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2019-10-29 06:12:08 +0900

    Add an interactive mode to tutorial/retrospective/prepare rb (#1335)
    * rename a local variable to make it more self-descriptive
    
    * Update the help message of retrospecitves/prepare.rb:
    
    Add an explanation about command line arguments of the script.
    
    * Add an interactive mode to retropsectives/prepare.rb:
    
    When you invoke the script with a location as its single argument,
    it enters an interactive mode, and allows you to specify the date of event
    and the type of yaml template files.
    
    * Add a workaround required under Git Bash for Windows:
    
    Calls of gets() are blocked under Git Bash for Windows, and you have to
    set STDOUT.sync to true to avoid the trouble.
    
    * Revert "Add a workaround required under Git Bash for Windows:"
    
    This reverts commit 1ff4753e55e082b38e1c124b67ac5d3160d600f6.
    
    The reverted reverted commit was for resolving an issue under
    Git Bash for Windows, but I will introduce a simpler workaround
    proposed by @kou .
    
    * Add a workaround required under Git Bash for Windows:
    
    Calls of gets() are blocked under Git Bash for Windows, and you have to
    set STDOUT.sync to true to avoid the trouble.
    (This workaround is proposed by @kou.)
    
    * Update tutorial/retrospectives/prepare.rb:
    
    Refactoring of set_arguments_interactively()
    
    Co-Authored-By: Sutou Kouhei <kou@cozmixng.org>
    
    * Refactoring of set_arguments_interactively()

--- tutorial/retrospectives/prepare.rb
+++ tutorial/retrospectives/prepare.rb
@@ -1,15 +1,65 @@
 #!/usr/bin/env ruby
 
-if ARGV.size < 2
-  puts("Usage: #{$0} DIRECTORY TYPE")
+if ARGV.size < 1
+  puts("Usage: #{$0} DIRECTORY TEMPLATE_TYPE")
   puts(" e.g.: #{$0} 2017-07-29-tokyo workshop")
+  puts("")
+  puts("Usage2: #{$0} LOCATION")
+  puts(" e.g.: #{$0} tokyo")
+  puts(" # With this usage, you can specify DATE and TEMPLATE_TYPE")
+  puts(" # interactively.")
+  puts("")
+  puts("# TEMPLATE_TYPE is used to specify the type of template files")
+  puts("# to be copied into DIRECTORY.")
+  puts("#  e.g.: ./workshop-beginner.yaml is copied to ./DIRECTORY/beginner.yaml ")
   exit(false)
 end
 
 require "fileutils"
+require "date"
 
-directory = ARGV.shift
-type = ARGV.shift
+def date_part(date = Date.today)
+  date.strftime("%Y-%m-%d")
+end
+
+def set_arguments_interactively(args)
+  args.each_value do |arg|
+    default = arg[:default]
+    print "#{arg[:label]} [#{default}]: "
+    $stdout.flush
+    value = gets.chomp
+    arg[:value] = value.empty? ? default : value
+  end
+end
+
+args = {
+  date: {
+    label: "DATE",
+    default: date_part,
+    value: nil
+  },
+#  location: {
+#    label: "LOCATION",
+#    default: nil,
+#    value: nil
+#  },
+  type: {
+    label: "TEMPLATE_TYPE",
+    default: "workshop",
+    value: nil
+  }
+}
+
+if ARGV.size == 1 # when a location is specified.
+  location = ARGV.shift
+  set_arguments_interactively(args)
+  directory = "#{args[:date][:value]}-#{location}"
+  type = args[:type][:value]
+else
+  directory = ARGV.shift
+  type = ARGV.shift
+end
+
 
 if File.directory?(directory)
   puts("Directory already exists: #{directory}")
@@ -18,6 +68,6 @@ end
 
 FileUtils.mkdir_p(directory)
 Dir.glob("#{type}-*.yaml").each do |yaml|
-  yaml_no_type = yaml.sub(/\A#{Regexp.escape(type)}-/, "")
-  FileUtils.cp(yaml, "#{directory}/#{yaml_no_type}")
+  yaml_without_type_prefix = yaml.sub(/\A#{Regexp.escape(type)}-/, "")
+  FileUtils.cp(yaml, "#{directory}/#{yaml_without_type_prefix}")
 end
