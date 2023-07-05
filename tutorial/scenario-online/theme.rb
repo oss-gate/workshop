9a8cd0026d0ff938e72dd25621286090f776635b
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2023-07-05 17:35:26 +0900

    Use specified fonts more certainly

--- tutorial/scenario-online/theme.rb
+++ tutorial/scenario-online/theme.rb
@@ -1,2 +1,5 @@
-@font_family = find_font_family("モトヤLマルベリ3等幅") || @font_family
+@font_family = find_font_family("モトヤLマルベリ3等幅") ||
+  find_font_family("モトヤLマルベリ") ||
+  find_font_family("MotoyaLMaru") ||
+  @font_family
 include_theme("clear-blue")
